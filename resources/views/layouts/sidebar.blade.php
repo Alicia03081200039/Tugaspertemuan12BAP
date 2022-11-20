3147a5122f8a9ad7284d72e55a736685efd41af3
Alicia03081200039 <03081200039@student.uph.edu>
2022-11-20 21:37:15 +0700

    upload

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,75 @@
+<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+    <!-- Add icons to the links using the .nav-icon class
+         with font-awesome or any other icon font library -->
+
+    <li class="nav-item">
+      <a href="http://127.0.0.1:8000/admin/attendance" class="nav-link">
+        <i class="far fa-file"></i>
+        <p>
+          Attendance
+          <span class="right badge badge-danger">New</span>
+        </p>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a href="http://127.0.0.1:8000/admin/schedule" class="nav-link">
+        <i class="fas fa-calendar-alt"></i>
+        <p>
+          Schedule
+          <span class="right badge badge-danger">New</span>
+        </p>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a href="http://127.0.0.1:8000/admin/user-role" class="nav-link">
+        <i class="fas fa-user-alt"></i>
+        <p>
+          User Role
+          <span class="right badge badge-danger">New</span>
+        </p>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a href="http://127.0.0.1:8000/admin/category" class="nav-link">
+        <i class="fas fa-ellipsis-v"></i>
+        <p>
+          Category
+          <span class="right badge badge-danger">New</span>
+        </p>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a href="http://127.0.0.1:8000/admin/reservation" class="nav-link">
+        <i class="far fa-chart-bar"></i>
+        <p>
+          Reservation
+          <span class="right badge badge-danger">New</span>
+        </p>
+      </a>
+    </li>
+
+      <li class="nav-item">
+        <a href="http://127.0.0.1:8000/admin/reservation-status" class="nav-link">
+          <i class="fas fa-calendar-check"></i>
+          <p>
+            Reservation Status
+            <span class="right badge badge-danger">New</span>
+          </p>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="http://127.0.0.1:8000/admin/missing-item-status" class="nav-link">
+          <i class="fas fa-times"></i>
+          <p>
+            Missing Item Status
+            <span class="right badge badge-danger">New</span>
+          </p>
+        </a>
+      </li>
+
+     
